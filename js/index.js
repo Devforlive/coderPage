f0e38301f6964f35915f66f6a5ed9fd974b353c1
«Devforlive» <e_mail_post_box@yahoo.com>
2017-11-10 20:22:39 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(function() {
   // Masonry Grid
     $('.grid').isotope({
     filter: '*',
-    // itemSelector: '.grid-item',
+        itemSelector: '.grid-item',
     masonry: {
 
       fitWidth: true, // When enabled, you can center the container with CSS.
