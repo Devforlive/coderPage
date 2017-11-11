7df165d9de65e52a5f9ed5d689cb52726dbe6f6f
«Devforlive» <e_mail_post_box@yahoo.com>
2017-11-11 15:05:52 +0200

    initial commit

--- js/map.js
+++ js/map.js
@@ -384,7 +384,7 @@
 
 var marker;
 
-function initMap() {
+$(function initMap() {
     var map = new google.maps.Map(document.getElementById("map-canvas"), {
         zoom: 7,
         center: {lat: 46.46117209999999, lng: 30.749565599999983}
@@ -396,9 +396,9 @@ function initMap() {
         icon: 'beetroot.png',
         animation: google.maps.Animation.DROP,
         position: {lat: 46.46117209999999, lng: 30.749565599999983}
-    });
+    }),
     marker.addListener('click', toggleBounce);
-}
+},
 
 function toggleBounce() {
     if (marker.getAnimation() !== null) {
@@ -406,5 +406,5 @@ function toggleBounce() {
     } else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
     }
-}
+});
 google.maps.event.addListener(window, 'load', initMap);
